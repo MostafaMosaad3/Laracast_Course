7085aea33ca9e7a5604adfe2279fa86589686d80
mostafa <mm9852803@gmail.com>
2023-10-19 07:17:45 +0300

    week 3

---
+++ week 3/app/Models/Post.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+    protected $fillable = ['title', 'slug', 'excerpt', 'body', 'category_id'];
+
+    protected $with= ['category' , 'author'] ;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author(){
+        return $this->belongsTo(User::class , 'user_id') ;
+    }
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false , fn($query , $search) => $query
+            ->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%')) ;
+
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+        $query->whereHas('category', fn ($query) =>
+        $query->where('slug', $category)
+        ));
+
+    }
+
+
+
+}
